a673cf8d3dcaeec9ca5b1f6afe3a2caee9963b43
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-10 12:57:04 -0400

    part 1 - arrays and objects cont.

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
   return (
     <div>
-      <h1> {props.course} </h1>
+      <h1> {props.course.name} </h1>
     </div>
   )
 }
@@ -15,6 +15,7 @@ const Content = (props) => {
       <Part part = {props.parts[0]} />
       <Part part = {props.parts[1]} />
       <Part part = {props.parts[2]} />
+      <Total exercises = {[props.parts[0].exercises, props.parts[1].exercises, props.parts[2].exercises]} />
     </div>
   )
 }
@@ -30,7 +31,6 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
-  console.log(props)
   return(
     <div>
       <p>{props.part.name} -- {props.part.exercises}</p>
@@ -39,28 +39,28 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const parts = [part1, part2, part3]
-  const exercises = [part1['exercises'], part2['exercises'], part3['exercises']]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total exercises={exercises} />
+      <Content parts={course.parts}/>
     </div>
   )
 }
