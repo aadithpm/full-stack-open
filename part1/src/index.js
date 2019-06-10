08fbe03cdf5f35a1107c51effcf4779bbb8c4548
Aadith Madapusi <amadapusi@industrydive.com>
2019-06-10 15:45:32 -0400

    part 1 - moving to stateful components

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,68 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1> {props.course.name} </h1>
-    </div>
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
   )
-}
-
-const Content = (props) => {
-  return(
-    <div>
-      <Part part = {props.parts[0]} />
-      <Part part = {props.parts[1]} />
-      <Part part = {props.parts[2]} />
-      <Total exercises = {[props.parts[0].exercises, props.parts[1].exercises, props.parts[2].exercises]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const add = (a, b) => a + b;
-
-  return(
-    <div>
-      Total exercises: {props.exercises.reduce(add)}
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return(
-    <div>
-      <p>{props.part.name} -- {props.part.exercises}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts}/>
+      {counter}
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
