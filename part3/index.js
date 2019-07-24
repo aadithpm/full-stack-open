aaca0fd7151f69f5b0efd152503e2266c38f434d
aadithpm <aadithpm@gmail.com>
2019-07-23 20:19:24 -0400

    Continuing with part3

--- part3/index.js
+++ part3/index.js
@@ -26,6 +26,17 @@ app.get('/notes', (req, res) => {
     res.json(notes)
 })
 
+app.get('/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const note = notes.find(note => note.id === id)
+    if(note){
+        res.json(note)
+    }
+    else{
+        res.status(404).end()
+    }
+})
+
 const port = 3001
 app.listen(port, () => {
     console.log(`Server: port ${port}`)
